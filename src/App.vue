664fffef4229de7ad7d0439c6bd007a51aad8291
BEATRICE <beatricechen2003@gmail.com>
2023-10-21 00:10:29 +0800

    Create reaction time game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>reaction timer</h1>
+  <button @click="start" :disable="isPlaying" >play!</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Block, Results},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
---
+++ src/components/Results.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="result">
+    <h1>here is your score</h1>
+    <p>{{score}} ms</p>
+    <p class="rank"> {{rank}} </p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data () {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Ninjia fingers'
+
+        } else if(this.score < 400) {
+            this.rank = 'rapid reflexes'
+        } else {
+            this.rank = 'snail pace hehe'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        color: red;
+    }
+
+</style>
